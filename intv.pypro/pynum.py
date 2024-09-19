359421f78790642c58d5d5c01bca74f122d15449
jagdish-git <62205654+jagdish-git@users.noreply.github.com>
2024-09-19 10:49:44 +0530

    Delete intv.pypro directory

--- intv.pypro/pynum.py
+++
@@ -1,5 +0,0 @@
-import numpy
-
-arrx = numpy.array([[3,4,5,6], [4,5,7,8]])
-print(len(arrx))
-print(arrx.size)
--- intv.pypro/test.py
+++
@@ -1,7 +0,0 @@
-lkey = ['a','b','c']
-tkey = (1,3,5,6)
-d = {}
-d[str(lkey)] = 345
-d[tkey] = 678
-for k,v in d.items():
-    print(k,'-',v)
